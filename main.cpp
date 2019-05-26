22781dec75c6ebc4f5ec4f57ad0c876a869c226b
Sebastian Kamraj <38919596+skamraj@users.noreply.github.com>
2019-05-26 23:54:02 +0200

    Changed

--- main.cpp
+++ main.cpp
@@ -19,6 +19,12 @@ public:
     int rj = 0; //Czas przygotowania
     int pj = 0; //Czas wykonania
     int qj = 0; //Czas dostarczen
+    /*Job(int R, int P, int Q)
+    {
+        this->R = R;
+        this->P = P;
+        this->Q = Q;
+    }*/
     void saveParameters() {
         cout << "Podaj kolejno numerprocesu, maszyne, czas:przygotowania,wykonania,dostarczenia." << endl;
         cin >> numberofproces;
@@ -60,7 +66,7 @@ public:
         return numberofproces;
     }
 
-   const int returnRj() {
+    int returnRj() {
         return rj;
     }
 
@@ -68,7 +74,7 @@ public:
         return pj;
     }
 
-   const int returnQj() {
+    int returnQj() {
         return qj;
     }
 
@@ -120,7 +126,7 @@ int ShrageAlgorithm(vector<Job> tab,int *tabhelp) {
 
             sort(helptab.begin(), helptab.end(),[] (const Job &a,const Job &b){return a.qj > b.qj;});
             auto it = helptab.begin();
-            time = time + it->returnPj();
+            time += it->returnPj();
             ro.push_back(*it);
 
            Cmax=max(Cmax,time+it->returnQj());
@@ -137,61 +143,108 @@ int ShrageAlgorithm(vector<Job> tab,int *tabhelp) {
 
     return Cmax;
 }
-int ShragePMTAlgorithm(vector<Job> tab,int *tabhelp1) {
-    int time = 0;
+
+
+
+int Schrage_pmtn(vector<Job> tab,int *tabhelp1)
+{
     int Cmax = 0;
-    int i=0;
-    vector<Job> helptab,tab1;
+    std::vector<Job> ready;
+    std::vector<Job> Permutation;
+    std::vector<Job> current_job;
+    std::vector<Job> temp_job;
+    Job example;
+    example.rj=0;
+    example.qj=0;
+    example.pj=0;
+    current_job.push_back(example);
+    temp_job.push_back(example);
 
+    int t = 0;
 
-    while (!helptab.empty() || !tab.empty()) {
-        auto itw=tab.begin();
-        while (!tab.empty() && itw->returnRj() <= time) {
-            helptab.push_back(*itw);
+    int temp = 0;
+    int temp_order = 0;
 
+    while (!ready.empty() || !tab.empty())
+    {
+        
+        while (!tab.empty() && tab.front().returnRj() <= t)
+        {
 
-            auto itw2=(itw+1);
-            if(itw->returnQj() > (itw2)->returnQj())
+            ready.push_back(tab.front());
+            temp_job.erase(temp_job.begin());
+            temp_job.push_back(tab.front());
+            tab.erase(tab.begin());
+
+            if (temp_job.front().returnQj() > current_job.front().returnQj())
             {
-                (itw2)->pj=time-(itw->returnRj());
-                time=itw->returnRj();
+                current_job.front().pj=(t - temp_job.front().returnRj());
+                t = temp_job.front().returnRj();
 
-                if((itw2)->returnPj() >0)
+
+                if (current_job.front().returnPj() > 0)
+                    ready.push_back(current_job.front());
+            }
+        }
+
+        if (ready.empty())
+            t = tab.front().returnRj();
+        else
+        {
+            if (ready.size() > 1)
+            {
+                for (int j = 0; j < ready.size(); j++)
                 {
-                    helptab.push_back(*itw2);
+                    if (ready[j].returnQj() >= temp)
+                    {
+                        temp = ready[j].returnQj();
+                        temp_order = j;
+                    }
                 }
             }
 
-            tab.erase(tab.begin());
-            itw=tab.begin();
+            Permutation.push_back(ready[temp_order]);	//add jobs to permutation
+            current_job.erase(current_job.begin());
+            current_job.push_back(ready[temp_order]);	//adding to current job
+
+            ready.erase(ready.begin() + temp_order);
+
+            t += Permutation.back().returnPj();
+            Cmax = max(Cmax, t + Permutation.back().returnQj());
+
+            temp = 0;
+            temp_order = 0;
         }
 
-        if (helptab.empty()) {
-            auto itpom=tab.begin();
-            time = itpom->returnRj();
+    }
+   // carus.Cmax_Schrage_pmtn=Cmax;
+    //cout << "\nCmax z przerwaniami: " << Cmax << std::endl;
+    return Cmax;
+}
+/*
+int bMax(vector<Job> tab,int Cmax){
+    for(int i=0;i<tab.size()-1;i++)
+    {
+        if(tab[i] == Cmax && )
+    }
+}*/
+int Carlier(vector<Job> tab, int *tabhelp, int np)
+{
+    int U,UB,LB;
+    int tabhelp1[np];
+    U=ShrageAlgorithm(tab,tabhelp);
 
-        } else {
-
-            sort(helptab.begin(), helptab.end(),[] (const Job &a,const Job &b){return a.qj > b.qj;});
-            auto it = helptab.begin();
-            time = time + it->returnPj();
-            Cmax=max(Cmax,time+it->returnQj());
-            tabhelp1[i]=it->getnumberproces();
-            i++;
-            helptab.erase(helptab.begin());
-        }
-
-
+    if(U < UB){
+        UB=U;
+        tabhelp=tabhelp1;
     }
 
 
-    return Cmax;
 }
-
 int main() {
     Job object;
     vector<Job> tab;
-    int nm, np, p1[100][100];
+    int nm, np, p1[500][500];
     ifstream data("data.txt");
     data >> np >> nm;
     int tabhelp1[np];
@@ -212,16 +265,20 @@ int main() {
         object.SaveRj(p1[j][i]);
         object.SavePj(p1[j + 1][i]);
         object.SaveQj(p1[j + 2][i]);
-
+     //   cout<<p1[j][i];
         tab.push_back(object);
 
     }
 
-
+   /* for(int i=0;i<np;i++)
+    {
+        cout<<tabhelp1[i]<<" ";
+    }
+    */
     sort(tab.begin(), tab.end(),[] (const Job &a,const Job &b){return a.rj < b.rj;});
 
     cout << "Wynik algorytmu to: " << ShrageAlgorithm(tab,tabhelp1) << endl;
-    cout << "Wynik algorytmuPMTA to: " << ShragePMTAlgorithm(tab,tabhelp2) << endl;
+    cout << "Wynik algorytmuPMTA to: " << Schrage_pmtn(tab,tabhelp2) << endl;
 
     return 0;
 }
